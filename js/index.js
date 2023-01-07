ac638ae4f48ddd768a852825df56dad73b34476f
lakdav <ladav2018@gmail.com>
2023-01-08 02:09:24 +0300

    refactor: refactor code

--- js/index.js
+++ js/index.js
@@ -1,56 +1,100 @@
-const dropdownList = document.querySelectorAll('.dropdown');
+const view = {
+	burger: document.getElementById('burger'),
+	navigation: document.getElementById('navigation'),
+	showNavigation: false,
+};
 
-let currentdropdownItem = null;
+view.burger.addEventListener('click', () => {
+	view.burger.classList.toggle('active');
+	view.navigation.classList.toggle('active');
+	view.navigation.style.setProperty(
+		'--height',
+		view.showNavigation ? 0 : view.navigation.firstElementChild.clientHeight + 'px',
+	);
 
-dropdownList.forEach((dropdown) => {
-	dropdown.addEventListener('click', () => {
-		if (currentdropdownItem === dropdown) {
-			dropdown.classList.toggle('active');
-			return;
-		}
-		if (currentdropdownItem) {
-			currentdropdownItem.classList.remove('active');
-		}
-		dropdown.classList.add('active');
-		currentdropdownItem = dropdown;
-	});
+	view.showNavigation = !view.showNavigation;
+	document.body.style.overflow = view.showNavigation ? 'hidden' : '';
+	document.documentElement.style.marginRight = view.showNavigation ? '1.5rem' : '';
+	view.burger.setAttribute('aria-expanded', view.showNavigation);
 });
-const custom_select = document.getElementById('custom-select');
-const select_toggle = document.getElementById('custom-select-toggle');
-const select_options = document.getElementById('custom-select-options');
-const options = document.querySelectorAll('.custom-select__option');
-const labelText = document.querySelector('.custom-select__label-text');
-const addressOutput = document.getElementById('address-output');
-const height = select_options.firstElementChild.getBoundingClientRect().height;
 
-select_toggle.addEventListener('click', () => {
-	custom_select.classList.toggle('active');
-	custom_select.style.setProperty('--height', height + 'px');
-	if (custom_select.classList.contains('active')) {
-		console.log('hiojkm');
-		return (addressOutput.style.display = 'none');
-	} else {
-		addressOutput.style.display = 'block';
+window.addEventListener('click', (e) => {
+	if (!e.target.closest('#burger')) {
+		view.burger.classList.remove('active');
+		view.navigation.classList.remove('active');
+		view.navigation.style.setProperty('--height', 0);
+		view.showNavigation = false;
+		view.burger.setAttribute('aria-expanded', false);
+		document.body.style.overflow = '';
+		document.documentElement.style.marginRight = '';
 	}
 });
 
-options.forEach((option) => {
-	option.addEventListener('click', () => {
-		custom_select.classList.remove('active');
+const accourdions = document.querySelectorAll('.accourdion');
+
+let currentaccourdion = null;
+
+accourdions.forEach((accourdion) => {
+	accourdion.addEventListener('click', (e) => {
+		if (e.target.closest('.accourdion__header')) {
+			const target = e.target.closest('.accourdion__header');
+			if (currentaccourdion === accourdion) {
+				target.setAttribute('aria-expanded', e.target.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
+				target.nextElementSibling.setAttribute(
+					'aria-hidden',
+					target.getAttribute('aria-expanded') === 'false' ? 'true' : 'false',
+				);
+				return;
+			}
+			if (currentaccourdion) {
+				currentaccourdion.firstElementChild.setAttribute('aria-expanded', false);
+				currentaccourdion.firstElementChild.nextElementSibling.setAttribute('aria-hidden', true);
+			}
+
+			target.setAttribute('aria-expanded', true);
+			target.nextElementSibling.setAttribute('aria-hidden', false);
+			currentaccourdion = accourdion;
+		}
+	});
+});
+
+const dropdown = document.getElementById('dropdown');
+const dropdownToggle = document.getElementById('dropdown-toggle');
+const dropdownPanel = document.getElementById('dropdown-panel');
+const menuitems = document.querySelectorAll('.dropdown__menuitem');
+const labelText = document.querySelector('.dropdown__label-text');
+const addressOutput = document.getElementById('address-output');
+const height = dropdown.firstElementChild.getBoundingClientRect().height;
+
+dropdownToggle.addEventListener('click', () => {
+	addressOutput.style.display = 'none';
+	const panel_hidden = dropdownPanel.getAttribute('aria-hidden');
+	const height = dropdownPanel.firstElementChild.clientHeight;
+	dropdownPanel.setAttribute('aria-hidden', panel_hidden === 'false' ? true : false);
+	dropdownToggle.setAttribute('aria-expanded', panel_hidden === 'false' ? false : true);
+	dropdown.style.setProperty('--height', panel_hidden === 'false' ? 0 : height + 'px');
+});
+
+menuitems.forEach((menuitem) => {
+	menuitem.addEventListener('click', () => {
+		dropdownPanel.setAttribute('aria-hidden', true);
+		dropdown.style.setProperty('--height', 0);
 		addressOutput.style.display = 'block';
-		labelText.textContent = option.dataset.city;
+		labelText.textContent = menuitem.dataset.city;
+		labelText.style.color = '#717171';
+		dropdownToggle.style.backgroundColor = '#C1E698';
 		addressOutput.innerHTML = `<div class="contacts__address address ml">
         <p class="address__city">
             <span class="field">City</span>
-            <span class="value">${option.dataset.city}</span>
+            <span class="value">${menuitem.dataset.city}</span>
         </p>
         <p class="address__phone">
             <span class="field">Phone:</span>
-            <span class="value">${option.dataset.phone}</span>
+            <span class="value">${menuitem.dataset.phone}</span>
         </p>
         <p class="address__office">
             <span class="field">Office adress:</span>
-            <span class="value">${option.dataset.officeadress}</span>
+            <span class="value">${menuitem.dataset.officeadress}</span>
         </p>
         <div class="address__call">
             <button class="btn-secondary btn-secondary-secondary--small">Call us</button>
@@ -59,7 +103,7 @@ options.forEach((option) => {
 	});
 });
 
-const filterButtons = document.querySelectorAll('.services__filter-item .btn');
+const filterButtons = document.querySelectorAll('.services__tablist-tab [role="tab"]');
 const services = document.querySelectorAll('.service');
 
 let currentButton = null;
@@ -71,8 +115,10 @@ filterButtons.forEach((btn) => {
 		}
 		if (currentButton) {
 			currentButton.classList.remove('active');
+			currentButton.setAttribute('aria-selected', false);
 		}
 		btn.classList.add('active');
+		btn.setAttribute('aria-selected', true);
 		services.forEach((service) => {
 			if (service.dataset.tag === btn.dataset.target) {
 				service.classList.remove('blur');
